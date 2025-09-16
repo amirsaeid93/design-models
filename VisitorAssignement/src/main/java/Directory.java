99117b1955429b0f6471bb7907d1c5268262190d
amirsaeid93 <said.taebnia@gmail.com>
2025-09-17 02:26:29 +0300

    Update

---
+++ VisitorAssignement/src/main/java/Directory.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory implements FileSystemElement {
+
+
+    private final String name;
+    private final List<FileSystemElement> children = new ArrayList<>();
+
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+
+    public void addElement(FileSystemElement element) {
+        children.add(element);
+    }
+
+
+    public List<FileSystemElement> getChildren() {
+        return children;
+    }
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public void accept(FileSystemVisitor visitor) {
+        visitor.visit(this);
+
+        for (FileSystemElement child : children) {
+            child.accept(visitor);
+        }
+    }
+
+}
+
